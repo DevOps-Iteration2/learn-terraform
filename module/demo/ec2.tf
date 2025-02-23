dbb6493e93260c48feb741818648c442b6dd71aa
Vivek <vivekanandareddy7677@gmail.com>
2025-02-22 19:45:46 -0600

    updating ami and sg

--- module/demo/ec2.tf
+++ module/demo/ec2.tf
@@ -1,9 +1,10 @@
 resource "aws_instance" "Demo" {
   ami           = ""
-  instance_type = "t3.micro"
+  instance_type = var.instance_type
   tags = {
     Name = var.Name
   }
 }
 
 variable "Name" {}
+variable "instance_type" {}
--- module/main.tf
+++ module/main.tf
@@ -1,8 +1,8 @@
+#Localpath_module
 module "demo-1" {
   source = "./demo"
   Name = "Demo-1"
+  instance_type = "t2.micro"
 }
 
-
-
-# Firstly we create an instance and later, if we intend to give name tag to it we declare variable block with a label "Name" in sub-module and get we provide value to declared variable it main.tf in root module
+# Firstly we create an instance and later, if we intend to give name tag to it we declare variable block with a label "Name" in sub-module and get we provide value to declared variable it main.tf
